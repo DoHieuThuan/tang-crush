66ed10335d67e74785d7f0d08af5333b1df9c490
DoHieuThuan <86537094+DoHieuThuan@users.noreply.github.com>
2021-06-27 04:56:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 const CONFIG = {
     introTitle: 'Bé Châu à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Em gì ơi? Em đánh rơi người yêu này`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Rõ ràng rồi',
+    btnNo: 'Không, đừng có mà tự tin :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
